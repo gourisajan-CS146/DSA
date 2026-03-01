b5273e6362160fa2fa8dabc44be483fa69b6b4e4
gourisajan-CS146 <gouri.sajan@sjsu.edu>
2026-02-28 21:28:56 -0800

    Update CountingSortForLetters.java

--- CountingSortForLetters.java
+++ CountingSortForLetters.java
@@ -5,15 +5,17 @@ public class CountingSortetters{
 
     char[] alphabetArr = { 'A','A','Z','B','H','K','L','Z','W','X','P'} ;
     int[] count = new int[26]; 
+   //convert letter to index letter-'A' and increase count when we see same letter
+   //count array will have count of each element in alphabet array
     for(int i = 0 ; i < alphabetArr.length ; i ++ ){ 
        count[alphabetArr[i]-'A']=count[alphabetArr[i]-'A']+1; 
      
     } 
-
+//print count array
    
 System.out.println("The count array is " + Arrays.toString(count));
 
-// build the cumulative count array.
+// build the cumulative count array. which will give count of element and elements less than that
 
 for ( int i = 1; i < 26 ; i++){
 
