b59312557e65dc72c274a13132054e3b3f8c1b76
gourisajan-CS146 <gouri.sajan@sjsu.edu>
2026-02-28 22:33:52 -0800

    Update BSTSearch.java

--- BSTSearch.java
+++ BSTSearch.java
@@ -2,17 +2,17 @@ import java.util.List;
 import java.util.ArrayList; 
 
 public class BSTSearch {
+  
   public static List<Integer> bstSearchTrace(TreeNode root, Integer k) { 
     // check if tree /subtree is empty  ://base con
     if ( root == null ) { 
       return  new ArrayList<Integer>();
-
     } 
   //an arraylist to hold only root
    List <Integer> rootList =  new ArrayList <Integer> () ;
    rootList.add( root.key );
     if (root.key == k ){
-      return rootList; 
+        return rootList; 
     } 
 
   //an arralist to store subtree  keys traversed while searching the K
@@ -24,18 +24,15 @@ public class BSTSearch {
        subNodesList  = bstSearchTrace(root.right, k)//pass right subtree as root
     } 
     //merge root and subnodes
-    rootList.addAll(subNodesList );
-  
-    return rootList;
-    
-
+    rootList.addAll(subNodesList ); 
+    return rootList;    
   } 
+  
 // helper method which creates BST to test 
   public static TreeNode createBST(){
     TreeNode rootNode = new rootNode(15);
     rootNode.left = new rootNode (6);
     rootNode.right = new rootNode (18);
-
 
     rootNode.left.left = new rootNode(3);
     rootNode.left.right = new rootNode(7);
@@ -48,7 +45,6 @@ public class BSTSearch {
 
     rootNode.right.left = new rootNode(17);
     rootNode.right.right = new rootNode(20);
-
     
     return rootNode; 
 
