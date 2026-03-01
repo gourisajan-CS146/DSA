a88006403dcdee8c87767320bd0bad20d35a4a25
gourisajan-CS146 <gouri.sajan@sjsu.edu>
2026-02-28 20:36:40 -0800

    Create CountingSortForLetters.java

---
+++ CountingSortForLetters.java
@@ -0,0 +1,16 @@
+import java.util.Arrays; 
+ 
+public class CountingSortetters{ 
+  public static void main (String args[]){
+
+    char[] alphabetArr = { 'A','A','Z','B','H','K','L','Z','W','X','P'} ;
+    int[] count = new int[26]; 
+    for(int i = 0 ; i < alphabetArr.length ; i ++ ){ 
+       count[alphabetArr[i]-'A']=count[alphabetArr[i]-'A']+1; 
+    } 
+
+  }   
+
+  
+} 
+
