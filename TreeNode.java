b1728aa8433dcb63956119d674f00dd8d7abbc12
gourisajan-CS146 <gouri.sajan@sjsu.edu>
2026-02-28 18:27:59 -0800

    Create TreeNode.java

---
+++ TreeNode.java
@@ -0,0 +1,17 @@
+public class TreeNode { 
+
+  int key; 
+
+  TreeNode left; 
+  TreeNode right; 
+
+  public TreeNode ( int key ) { 
+
+    this.key = key; 
+    this.left = null; 
+    this.right = null; 
+  } 
+
+  
+
+} 
