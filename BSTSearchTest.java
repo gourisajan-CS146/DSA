856a5f642eb66de340e3f73a2c06023337908dae
gourisajan-CS146 <gouri.sajan@sjsu.edu>
2026-02-28 20:03:11 -0800

    Update BSTSearchTest.java

--- BSTSearchTest.java
+++ BSTSearchTest.java
@@ -5,18 +5,42 @@ import org.junit.jupiter.api.Test;
 public class BSTSearchTest{ 
 
 @Test 
-public void searchExistingKey(){
-
-TreeNode root = BSTSearch.createBST();
-
-List<Integers> expList = Arrays.asList(15,6,7,13);
-
-List<Integer>   actualList = BSTSearch.bstSearchTrace(root, 13);
-assertEquals(expList,actualList,"Trace to find 13 is incorrect");
-
-
+public void  testSearchExistingKey(){
+    TreeNode root = BSTSearch.createBST();
+    List<Integers> expList = Arrays.asList(15,6,7,13);
+    List<Integer>   actualList = BSTSearch.bstSearchTrace(root, 13);
+    assertEquals(expList,actualList,"Trace to find 13 is incorrect");
 } 
 
+
+  @Test 
+public void testsearchNonExistingKey(){
+    TreeNode root = BSTSearch.createBST();
+    List<Integers> expList = Arrays.asList(15,6,7,13);
+    List<Integer>   actualList = BSTSearch.bstSearchTrace(root, 14);
+    assertEquals(expList,actualList,"Trace to find 14 which doesnot exist is incorrect");
+} 
+
+   @Test 
+public void testsearchforRoot(){
+    TreeNode root = BSTSearch.createBST();
+    List<Integers> expList = Arrays.asList(15);
+    List<Integer>   actualList = BSTSearch.bstSearchTrace(root, 15);
+    assertEquals(expList,actualList,"Trace to find root 15 is incorret " );
+} 
+
+   @Test 
+public void testsearchforleftMostKey(){
+    TreeNode root = BSTSearch.createBST();
+    List<Integers> expList = Arrays.asList(15,6,3,2);
+    List<Integer>   actualList = BSTSearch.bstSearchTrace(root, 2);
+    assertEquals(expList,actualList,"Trace to find root 2 is incorret " );
+} 
+
+  
+
+
+  
 
 
 } 
