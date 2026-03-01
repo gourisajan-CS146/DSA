09ffda5c68720da085ee3151946ff1ad0cb0b415
gourisajan-CS146 <gouri.sajan@sjsu.edu>
2026-02-28 22:42:55 -0800

    Update BSTSearchTest.java

--- BSTSearchTest.java
+++ BSTSearchTest.java
@@ -7,7 +7,7 @@ public class BSTSearchTest{
         @Test 
         public void  testSearchExistingKey(){
             TreeNode root = BSTSearch.createBST();
-            List<Integers> expList = Arrays.asList(15,6,7,13);
+            List<Integer> expList = Arrays.asList(15,6,7,13);
             List<Integer>   actualList = BSTSearch.bstSearchTrace(root, 13);
             assertEquals(expList,actualList,"Trace to find 13 is incorrect");
         } 
@@ -16,7 +16,7 @@ public class BSTSearchTest{
        @Test 
         public void testsearchNonExistingKey(){
             TreeNode root = BSTSearch.createBST();
-            List<Integers> expList = Arrays.asList(15,6,7,13);
+            List<Integer> expList = Arrays.asList(15,6,7,13);
             List<Integer>   actualList = BSTSearch.bstSearchTrace(root, 14);
             assertEquals(expList,actualList,"Trace to find 14 which doesnot exist is incorrect");
         } 
@@ -24,7 +24,7 @@ public class BSTSearchTest{
         @Test 
         public void testsearchforRoot(){
             TreeNode root = BSTSearch.createBST();
-            List<Integers> expList = Arrays.asList(15);
+            List<Integer> expList = Arrays.asList(15);
             List<Integer>   actualList = BSTSearch.bstSearchTrace(root, 15);
             assertEquals(expList,actualList,"Trace to find root 15 is incorret " );
         } 
@@ -32,7 +32,7 @@ public class BSTSearchTest{
        @Test 
         public void testsearchforleftMostKey(){
             TreeNode root = BSTSearch.createBST();
-            List<Integers> expList = Arrays.asList(15,6,3,2);
+            List<Integer> expList = Arrays.asList(15,6,3,2);
             List<Integer>   actualList = BSTSearch.bstSearchTrace(root, 2);
             assertEquals(expList,actualList,"Trace to find root 2 is incorret " );
         } 
@@ -40,7 +40,7 @@ public class BSTSearchTest{
         @Test 
         public void testsearchforRightMostKey(){
             TreeNode root = BSTSearch.createBST();
-            List<Integers> expList = Arrays.asList(15,18,20);
+            List<Integer> expList = Arrays.asList(15,18,20);
             List<Integer>   actualList = BSTSearch.bstSearchTrace(root, 20);
             assertEquals(expList,actualList,"Trace to find root 20 is incorret " );
         } 
