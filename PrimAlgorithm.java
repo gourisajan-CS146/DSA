dc85df9cd4927530f2bcf9c60a69e630021fc6c4
gourisajan-CS146 <gouri.sajan@sjsu.edu>
2026-03-25 21:48:41 -0700

    Update PrimAlgorithm.java

--- PrimAlgorithm.java
+++ PrimAlgorithm.java
@@ -1,4 +1,14 @@
 public class PrimAlgorithm{
+ static class GraphEdge {
+        int destVetex,;
+       int weight;
+
+        GrapghEdge(int destVetex, int weight) {
+            this.destVetex = destVetex;
+            this.weight = weight;
+        }
+    }
+
 
 static final int V = 9;//no of vertices 
   public static findMinimumWeightedEdge(int[] key,boolean[] inMST){
